d6453c9e38f89ed7bfb14990e3b5c5ee586f073d
rashmiKumari03 <rashmi15497@gmail.com>
2025-01-03 22:48:17 +0530

    code in artifacts_entity.py (class Data_Validation_Artifacts)

--- src/Shipment_Price_Prediction/entity/artifacts_entity.py
+++ src/Shipment_Price_Prediction/entity/artifacts_entity.py
@@ -18,3 +18,27 @@ class Data_Ingestion_Artifacts:
     """
     train_data_file_path: str
     test_data_file_path: str
+    
+    
+    
+# Data Validation Artifacts
+
+@dataclass
+class Data_Validation_Artifacts:
+    """
+    This class holds the information and file paths generated during the data validation process.
+
+    Attributes:
+    - data_drift_file_path (str): The path to the file that contains the data drift report. 
+      This report indicates if there is any significant difference between the distribution of training and testing data or between new data and the original dataset.
+    - validation_status (bool): A flag indicating whether the data validation process was successful or not. 
+      If True, the data passed all validation checks (e.g., schema validation, missing value checks, etc.). 
+      If False, issues were found during the validation process.
+
+    Purpose:
+    After the data validation process, artifacts such as the data drift report and validation status are generated. 
+    This class helps in storing and accessing this information for further steps in the machine learning pipeline, 
+    ensuring the data is reliable and ready for model training or evaluation.
+    """
+    data_drift_file_path: str
+    validation_status: bool
