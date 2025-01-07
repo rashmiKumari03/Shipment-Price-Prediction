fd767250a06ffb263aa372ca1f0deb4aaa733ae3
rashmiKumari03 <rashmi15497@gmail.com>
2025-01-08 00:26:18 +0530

    added code for transformation config to config_entity.py

--- src/Shipment_Price_Prediction/entity/config_entity.py
+++ src/Shipment_Price_Prediction/entity/config_entity.py
@@ -95,8 +95,23 @@ class Data_Validation_Config:
         self.DATA_DRIFT_FILE_PATH: str = os.path.join(self.DATA_VALIDATION_ARTIFACTS_DIR, DATA_DRIFT_FILE_NAME)
         
         
+# class Data_Transformation_Config : Configuration class for the data transformation process.
+class Data_Transformation_Config :
+    def __init__(self):
         
+        self.UTILS = MainUtils()
         
+        self.SCHEMA_CONFIG = self.UTILS.read_yaml_file(filename=SCHEMA_FILE_PATH)
         
+        self.DATA_INGESTION_ARTIFACTS_DIR:str = os.path.join(from_root(),ARTIFACTS_DIR,DATA_INGESTION_ARTIFACTS_DIR)
+        self.DATA_TRANSFORMATION_ARTIFACTS_DIR:str = os.path.join(from_root(),ARTIFACTS_DIR,DATA_TRANSFORMATION_ARTIFACTS_DIR)
+        
+        self.TRANSFORMED_TRAIN_DATA_DIR:str = os.path.join(self.DATA_TRANSFORMATION_ARTIFACTS_DIR,TRANSFORMED_TRAIN_DATA_DIR)
+        self.TRANSFORMED_TEST_DATA_DIR:str = os.path.join(self.DATA_TRANSFORMATION_ARTIFACTS_DIR,TRANSFORMED_TEST_DATA_DIR)
+        
+        self.TRANSFORMED_TRAIN_FILE_PATH:str = os.path.join(self.DATA_TRANSFORMATION_ARTIFACTS_DIR,TRANSFORMED_TRAIN_DATA_FILE_NAME)
+        self.TRANSFORMED_TEST_FILE_PATH:str = os.path.join(self.DATA_TRANSFORMATION_ARTIFACTS_DIR,TRANSFORMED_TEST_DATA_FILE_NAME)
+        
+        self.PREPROCESSOR_FILE_PATH:str = os.path.join(from_root(),ARTIFACTS_DIR,DATA_TRANSFORMATION_ARTIFACTS_DIR,PREPROCESSOR_OBJECT_FILE_NAME)
         
         
