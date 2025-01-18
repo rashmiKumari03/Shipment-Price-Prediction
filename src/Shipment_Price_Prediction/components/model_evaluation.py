b73e5ff4d4c51ef4811d183104509ce0643fc213
rashmiKumari03 <rashmi15497@gmail.com>
2025-01-18 22:36:12 +0530

    update in model_evaluation.py

--- src/Shipment_Price_Prediction/components/model_evaluation.py
+++ src/Shipment_Price_Prediction/components/model_evaluation.py
@@ -8,4 +8,6 @@ from src.Shipment_Price_Prediction.constant import *
 from src.Shipment_Price_Prediction.entity.artifacts_entity import (Data_Ingestion_Artifacts,Model_Trainer_Artifacts,Model_Evaluation_Artifacts)
 from src.Shipment_Price_Prediction.entity.config_entity import Model_Evaluation_Config
 
+# Need to create some constants in constant folder for model evaluation.
+# Writing the configuration regarding S3 Bucket.
 
