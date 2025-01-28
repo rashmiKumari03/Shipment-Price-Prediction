dba15e5e86c4375e221d6f6dd3fd358772c9a954
rashmiKumari03 <rashmi15497@gmail.com>
2025-01-28 23:21:22 +0530

    code in model_pusher.py

--- src/Shipment_Price_Prediction/components/model_pusher.py
+++ src/Shipment_Price_Prediction/components/model_pusher.py
@@ -6,3 +6,7 @@ from src.Shipment_Price_Prediction.configuration.s3_operation import S3_Operatio
 from src.Shipment_Price_Prediction.entity.artifacts_entity import (Data_Transformation_Artifacts , Model_Trainer_Artifacts , Model_Pusher_Artifacts)
 
 from src.Shipment_Price_Prediction.entity.config_entity import Model_Pusher_Config
+
+# Lets  Create first the Model_Pusher_Artifacts and Model_Pusher_Config
+
+
