a38c3efbe0ff1e1d49031b374109d73d6dfe7390
rashmiKumari03 <rashmi15497@gmail.com>
2025-01-29 00:12:34 +0530

    code added to model_pusher.py

--- src/Shipment_Price_Prediction/components/model_pusher.py
+++ src/Shipment_Price_Prediction/components/model_pusher.py
@@ -9,4 +9,40 @@ from src.Shipment_Price_Prediction.entity.config_entity import Model_Pusher_Conf
 
 # Lets  Create first the Model_Pusher_Artifacts and Model_Pusher_Config
 
-
+class Model_Pusher:
+    
+    def __init__(self, 
+                 model_pusher_config : Model_Pusher_Config ,
+                 model_trainer_artifacts : Model_Trainer_Artifacts , 
+                 data_transformation_artifacts: Data_Transformation_Artifacts,
+                 s3: S3_Operation
+                 ):
+        
+        self.model_pusher_config = model_pusher_config
+        self.model_trainer_artifacts = model_trainer_artifacts
+        self.data_transformation_artifacts = data_transformation_artifacts
+        self.s3 = s3
+        
+        
+    # This is method is used to initiate the model pusher.
+    def initiate_model_pusher(self) -> Model_Pusher_Artifacts:
+        """ 
+        Method Name : initiate_model_pusher
+        
+        Description : This method initiates model pusher.
+        
+        Output      : Model Pusher Artifacts
+        """
+        logging.info("Entered the initiate_model_pusher method of Model_Pusher class")
+        try:
+            # Uploading the best model to S3 Bucket
+            
+            self.s3.upload_file(self.model_trainer_artifacts.trained_model_file_path,
+                                self.model_pusher_config.S3_MODEL_KEY_PATH,
+                                self.model_pusher_config.BUCKET_NAME,
+                                remove=False
+                                )
+            logging.info("Uploaded the best model to S3 Bucket")
+            logging.info("Exited the initiate_model_pusher method of Model_Pusher class")             
+        
+    
