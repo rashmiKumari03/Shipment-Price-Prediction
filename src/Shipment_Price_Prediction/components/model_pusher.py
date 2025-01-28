7e7d3d0c3827571c13dd03a2fd1b9b4c469f8bf6
rashmiKumari03 <rashmi15497@gmail.com>
2025-01-28 23:11:44 +0530

    code added to model_pusher.py

---
+++ src/Shipment_Price_Prediction/components/model_pusher.py
@@ -0,0 +1,8 @@
+import sys
+from src.Shipment_Price_Prediction.logger import logging
+from src.Shipment_Price_Prediction.exception import CustomException
+
+from src.Shipment_Price_Prediction.configuration.s3_operation import S3_Operation
+from src.Shipment_Price_Prediction.entity.artifacts_entity import (Data_Transformation_Artifacts , Model_Trainer_Artifacts , Model_Pusher_Artifacts)
+
+from src.Shipment_Price_Prediction.entity.config_entity import Model_Pusher_Config
