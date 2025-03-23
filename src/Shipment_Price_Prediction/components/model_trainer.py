bd0626b1b4effe1dd2dad5cce3a4da556862f1f8
rashmiKumari03 <rashmi15497@gmail.com>
2025-03-23 18:18:16 +0530

    updated model_trainer.py

--- src/Shipment_Price_Prediction/components/model_trainer.py
+++ src/Shipment_Price_Prediction/components/model_trainer.py
@@ -18,28 +18,26 @@ class Cost_Model:
     def __init__(self, preprocessing_object: object, trained_model_object: object):
         self.preprocessing_object = preprocessing_object
         self.model = trained_model_object
-
         
-        UTILS = MainUtils()
-        self.SCHEMA_CONFIG = UTILS.read_yaml_file(filename=SCHEMA_FILE_PATH)
-        self.numeric_columns = self.SCHEMA_CONFIG.get('numeric_features',[])
 
-    def predict(self, X_transformed: pd.DataFrame) -> np.ndarray: 
+    def prediction(self, X:DataFrame):
         """
         Predict shipment costs based on input features.
         """
         logging.info("Entered the 'predict' method of the Cost_Model class.")
         try:
+            logging.info(f"X looks like : \n {X.head()}")
+            # Preprocess the input data
             
-            logging.info(f"Data shape after conversion: {X_transformed.shape}")
-            logging.info(f"Sample data after conversion:\n{X_transformed[:5]}")
-
-            # Predictions
-            predictions = self.model.predict(X_transformed)
-            logging.info("Predictions generated successfully.....from Cost_Model Class")
-            logging.info(f"The Prediction is : {predictions}")
-
-            return predictions
+            # Loading the preprocessor.pkl
+            logging.info(f"Preprocessor and Model object provided in the predict..")
+            
+            logging.info(f"preprocessor_obj is :{self.preprocessing_object}")
+            X_transformed = X.drop_duplicates()
+            X_transformed = self.preprocessing_object.transform(X)
+            # Make predictions using the trained model
+            logging.info("Transformation on new/test data has Completed!")
+            return self.model.predict(X_transformed)
             
 
         except Exception as e:
@@ -61,6 +59,13 @@ class Model_Trainer:
         
         self.data_transformation_artifact = data_transformation_artifact
         self.model_trainer_config = model_trainer_config
+        
+         
+        UTILS = MainUtils()
+        self.SCHEMA_CONFIG = UTILS.read_yaml_file(filename=SCHEMA_FILE_PATH)
+        self.numeric_columns = self.SCHEMA_CONFIG.get('numeric_features',[])
+        
+        
     
     # This method is used to get the trained model.
     def get_trained_models(self, train_data: DataFrame, test_data: DataFrame) -> List[Tuple[float, object, str]]:
@@ -82,17 +87,20 @@ class Model_Trainer:
             Usually, splitting occurs when both the independent and dependent data are in the same training set. 
             However, in this case, since my independent and dependent data are already saved in separate files in .npz format, 
             I will load them directly."
-            '''
             
-            x_train = train_data
-            x_test = test_data
-            y_train = self.model_trainer_config.UTILS.load_numpy_array_data(self.data_transformation_artifact.y_transformed_train_file_path)
-            y_test = self.model_trainer_config.UTILS.load_numpy_array_data(self.data_transformation_artifact.y_transformed_test_file_path)
+            '''
             
             logging.info(f"Training data: {train_data.shape}, Test data: {test_data.shape}")
             
+            X_train , y_train ,   X_test , y_test  = (train_data.drop(train_data.columns[len(train_data.columns)-1],axis=1) ,
+                                                      train_data.iloc[:,-1] ,
+                                                      test_data.drop(test_data.columns[len(test_data.columns)-1],axis=1) , 
+                                                      test_data.iloc[:,-1])         
+            
+          
+            
             trained_models = [
-                self.model_trainer_config.UTILS.get_tunned_model(model_name,x_train, y_train,x_test, y_test)
+                self.model_trainer_config.UTILS.get_tunned_model(model_name,X_train, y_train,X_test, y_test)
                 for model_name in models_list
             ]
 
@@ -120,14 +128,14 @@ class Model_Trainer:
         try:
             os.makedirs(self.model_trainer_config.MODEL_TRAINER_ARTIFACTS_DIR, exist_ok=True)
             
-            X_train = pd.DataFrame(self.model_trainer_config.UTILS.load_numpy_array_data(self.data_transformation_artifact.X_transformed_train_file_path))
-            X_test = pd.DataFrame(self.model_trainer_config.UTILS.load_numpy_array_data(self.data_transformation_artifact.X_transformed_test_file_path))
+            train_arr = pd.DataFrame(self.model_trainer_config.UTILS.load_numpy_array_data(self.data_transformation_artifact.train_file_path))
+            test_arr = pd.DataFrame(self.model_trainer_config.UTILS.load_numpy_array_data(self.data_transformation_artifact.test_file_path))
             
-            logging.info(f"X_train looks like : {X_train.head()}")
-            logging.info(f"X_test looks like : {X_test.head()}")
+            logging.info(f"X_train looks like : {train_arr.head()}")
+            logging.info(f"X_test looks like : {test_arr.head()}")
             logging.info("===========================================")
 
-            trained_models = self.get_trained_models(X_train, X_test)
+            trained_models = self.get_trained_models(train_arr, test_arr)
             
             best_model_name, best_model_score, best_model_object = self.model_trainer_config.UTILS.get_best_model_with_name_and_score(trained_models)
             
@@ -138,12 +146,14 @@ class Model_Trainer:
                 self.model_trainer_config.UTILS.update_model_score(best_model_score)
                 
                 preprocessor_obj = self.model_trainer_config.UTILS.load_object(self.data_transformation_artifact.transformed_object_file_path)
+                logging.info("Here i am Saving the cost_model using save_object!!!!")
                 cost_model = Cost_Model(preprocessor_obj, best_model_object)
-                
+                logging.info(f"cost_model looks like : {cost_model} and type of cost_model is {type(cost_model)}")
                 model_file_path = self.model_trainer_config.UTILS.save_object(self.model_trainer_config.TRAINED_MODEL_FILE_PATH, cost_model)
-                
+                logging.info(f"model_file_path looks like : {model_file_path}")
                 model_trainer_artifacts = Model_Trainer_Artifacts(trained_model_file_path=model_file_path)
                 logging.info(f"Model saved at {model_file_path}")
+                logging.info(f"Model Trainer Artifacts : {model_trainer_artifacts}")
 
                 return model_trainer_artifacts
             
